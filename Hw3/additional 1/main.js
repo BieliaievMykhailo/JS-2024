7e13ced8a1bdfc4d84df6e30b3149c481c9cb0e1
BieliaievMykhailo <bieliaiev.mykhailo@gmail.com>
2024-08-12 16:22:23 +0300

    additional 1 done

--- Hw3/additional 1/main.js
+++ Hw3/additional 1/main.js
@@ -133,3 +133,110 @@ for (let j = arr2.length - 1; j >= 0; j--) {
     console.log(arr2[j]);
 }
 console.log('_________________________________');
+
+// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+let arr3=[1,2,3,4,5,6,7,8,9,10];
+for (const number of arr3) {
+    console.log(number);
+}
+console.log('_________________________________');
+
+// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+let string_data3 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+for (const string of string_data3) {
+    console.log(string);
+}
+console.log('_________________________________');
+
+// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+let string_data4 = ['one', true, true, 'four', 'five', 6, 7, 'eight', 'nine', 'ten'];
+for (const stringData4Element of string_data4) {
+    console.log(stringData4Element);
+}
+console.log('_________________________________');
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+let string_data5 = ['one', true, true, 'four', 'five', 6, 7, 'eight', 'nine', 'ten'];
+for (const stringData5Element of string_data5) {
+    if (typeof stringData5Element === "boolean"){
+        console.log(stringData5Element);
+    }
+}
+console.log('_________________________________');
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+for (const stringData5Element of string_data5) {
+    if (typeof stringData5Element === "number"){
+        console.log(stringData5Element);
+    }
+}
+console.log('_________________________________');
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+for (const stringData5Element of string_data5) {
+    if (typeof stringData5Element === "string"){
+        console.log(stringData5Element);
+    }
+}
+console.log('_________________________________');
+
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+//     Вивести в консоль всі його елементи в циклі.
+let arr5 = [];
+for (let j = 0; j < 10; j++) {
+    if (j%2===0){
+        arr5[j]=j;
+    }else arr5[j]=`${'string'} ${j}`;
+}
+console.log(arr5);
+console.log('_________________________________');
+
+// - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let j = 0; j < 10; j++) {
+    document.write(j+' ');
+    console.log(j);
+}
+console.log('_________________________________');
+document.write(`<br>`);
+// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let j = 0; j < 100; j++) {
+    document.write(j+' ');
+    console.log(j);
+}
+console.log('_________________________________');
+document.write(`<br>`);
+
+// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+for (let j = 0; j < 200; j=j+2) {
+    document.write(j+' ');
+    console.log(j);
+}
+console.log('_________________________________');
+document.write(`<br>`);
+
+// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+for (let j = 0; j < 100; j++) {
+    if (j%2===0) {
+        document.write(j + ' ');
+        console.log(j);
+    }
+}
+console.log('_________________________________');
+document.write(`<br>`);
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+for (let j = 0; j < 100; j++) {
+    if (j%2!==0) {
+        document.write(j + ' ');
+        console.log(j);
+    }
+}
+console.log('_________________________________');
+
+// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+let books = [
+    {title:'book1', str:50, author:'author1',}
+];
+// -знайти наібльшу книжку.
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор
