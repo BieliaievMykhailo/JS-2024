ef24e3c878e156cfa3784c2b14051bdfb3da0943
BieliaievMykhailo <bieliaiev.mykhailo@gmail.com>
2024-08-19 16:09:12 +0300

    Hw 6

--- Hw6/main.js
+++ Hw6/main.js
@@ -39,39 +39,119 @@ console.log('______________________________________');
 
 // #Rbr5kEQ
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
-arr =[10,8,-7,55,987,-1011,0,1050,0];
-let mapResultArray = arr.map((item ) => (item.toString()));
+arr = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+let mapResultArray = arr.map((item) => (item.toString()));
 console.log(mapResultArray);
 console.log('______________________________________');
+
 // #5hqyKTfmc
 // - створити функцію sortNums(array,direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
 // або навпаки в залежності від значення аргументу direction.
-    let nums = [11,21,3];
+let nums = [11, 21, 3];
 // sortNums(nums,'ascending') // [3,11,21]
-// sortNums(nums,'descending') // [21,11,3]
+// sortNums(nums,'ç') // [21,11,3]
+function sortNums(array, direction) {
+    if (direction === 'ascending') {
+        nums.sort((a, b) => (a - b));
+        return nums;
+    } else if (direction === 'descending') {
+        nums.sort((a, b) => (b - a));
+        return nums;
+    }
+}
+
+console.log(sortNums(nums, 'ascending'));
+
+console.log(sortNums(nums, 'descending'));
+console.log('______________________________________');
 
 // ==========================
 // #yo06d74c1C
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // -- відсортувати його за спаданням за monthDuration
+coursesAndDurationArray.sort((a, b) => (b.monthDuration - a.monthDuration));
+console.log(coursesAndDurationArray);
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let resultFilterArray = coursesAndDurationArray.filter(course => course.monthDuration > 5);
+console.log(resultFilterArray);
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+mapResultArray = coursesAndDurationArray.map((course, index) => {
+    return {
+        id: index + 1,
+        title: course.title,
+        monthDuration: course.monthDuration,
+    };
+});
+console.log(mapResultArray);
+console.log('______________________________________');
+
 // =========================
 //     #bolvdlhP
 // описати колоду карт (від 6 до туза без джокерів)
+let cardSuits = ['spade', 'diamond', 'heart', 'clubs'];
+let values = ['6', '7', '8', '9', '10', 'ace', 'jack', 'queen', 'king'];
+let deckOfCards = [];
+for (const cardSuit of cardSuits) {
+    for (const value of values) {
+        deckOfCards.push({cardSuit: cardSuit, value: value});
+    }
+}
+for (const item of deckOfCards) {
+    if (item.cardSuit === 'spade' || item.cardSuit === 'clubs') {
+        item.color = 'black';
+    } else {
+        item.color = 'red';
+    }
+}
+console.log(deckOfCards);
 // - знайти піковий туз
+for (const item of deckOfCards) {
+    if (item.cardSuit === 'spade' && item.value === 'ace') {
+        console.log(item);
+    }
+}
+console.log('______________________________________');
+
 // - всі шістки
+for (const item of deckOfCards) {
+    if (item.value === '6') {
+        console.log(item);
+    }
+}
+console.log('______________________________________');
+
 // - всі червоні карти
+for (const item of deckOfCards) {
+    if (item.color === 'red') {
+        console.log(item);
+    }
+}
+console.log('______________________________________');
+
 // - всі буби
+for (const item of deckOfCards) {
+    if (item.cardSuit === 'diamond') {
+        console.log(item);
+    }
+}
+console.log('______________________________________');
+
 // - всі трефи від 9 та більше
+for (const item of deckOfCards) {
+    if (item.cardSuit === 'clubs' && (+item.value >= 9 || item.value === 'jack' || item.value === 'queen' || item.value === 'king' || item.value === 'ace')) {
+        console.log(item);
+    }
+}
+console.log('______________________________________');
+
 //
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
