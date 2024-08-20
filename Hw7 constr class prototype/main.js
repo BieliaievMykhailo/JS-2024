980ddf7a9523384ed7267176061574ce9d17cb05
BieliaievMykhailo <bieliaiev.mykhailo@gmail.com>
2024-08-20 18:52:57 +0300

    hw 7 done!

--- Hw7 constr class prototype/main.js
+++ Hw7 constr class prototype/main.js
@@ -105,7 +105,7 @@ function Car(model, manufacturer, year, maxSpeed, engine) {
         this.year = newValue;
         console.log(`year - ${this.year}`);
     };
-    this.addDriver = function (driver){
+    this.addDriver = function (driver) {
         this.driver = driver;
     }
 }
@@ -129,16 +129,120 @@ console.log('____________________________________________________________-');
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-//
-//
+class Car2 {
+    constructor(model, manufacturer, year, maxSpeed, engine) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    };
+
+    drive() {
+        console.log(`${this.maxSpeed}`)
+    };
+
+    info() {
+        console.log(`model -  ${this.model}, manufacturer - ${this.manufacturer}, 
+        year - ${this.year}, max speed - ${this.maxSpeed}, engine - ${this.engine}`)
+    };
+
+    increaseMaxSpeed(speed) {
+        let newSpeed = this.maxSpeed + speed;
+        console.log(`New max speed is ${newSpeed}km/h`);
+    };
+
+    changeYear(newValue) {
+        this.year = newValue;
+        console.log(`year - ${this.year}`);
+    };
+
+    addDriver(driver) {
+        this.driver = driver;
+    }
+}
+
+let car2 = new Car2('jetta', 'WV', 2015, 260, 1400);
+console.log(car2);
+car2.drive();
+car2.info();
+car2.increaseMaxSpeed(10);
+car2.changeYear(2000);
+car2.addDriver({name: 'Vasya', age: 45});
+console.log(car2);
+console.log('____________________________________________________________-');
+
 //
 // #zg6Fifnqig
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+class Cinderella {
+    constructor(name, age, size) {
+        this.name = name;
+        this.age = age;
+        this.size = size;
+    };
+}
+
+let cinderellas = [
+    new Cinderella('cind1', 18, 37),
+    new Cinderella('cind2', 14, 36),
+    new Cinderella('cind3', 17, 34),
+    new Cinderella('cind4', 16, 35),
+    new Cinderella('cind5', 23, 36),
+    new Cinderella('cind6', 34, 37),
+    new Cinderella('cind7', 48, 38),
+    new Cinderella('cind8', 13, 39),
+    new Cinderella('cind9', 54, 43),
+    new Cinderella('cind10', 38, 45),
+]
+console.log(cinderellas);
+
+class Prince {
+    constructor(name, age, shoeToSearch) {
+        this.name = name;
+        this.age = age;
+        this.shoeToSearch = shoeToSearch;
+    };
+}
+
+let prince = new Prince('Vasya', 25, 34);
+console.log(prince);
+
+for (const item of cinderellas) {
+    if (item.size === prince.shoeToSearch) {
+        console.log('Prince, your cinderella is - ', item);
+    }
+}
+
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+let cinderellaToSearch = cinderellas.find(value => value.size === prince.shoeToSearch);
+console.log(cinderellaToSearch);
+console.log('_______________________________');
 //
 //
 //
 // #gsKLAsNWM
 // *Через Array.prototype. створити власний foreach, filter
+Array.prototype.newForeEach = function (callback) {
+    let arr = this;
+    for (const arrElement of arr) {
+        callback(arrElement);
+    }
+};
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+array.newForeEach((x) => console.log(x));
+console.log('_______________________________');
+
+Array.prototype.newFilter = function (callback) {
+    let arr2 = [];
+    for (const item of this) {
+        if (callback(item)) {
+            arr2.push(item);
+        }
+    }
+    return arr2;
+};
+let c = cinderellas.newFilter((item) => item.age < 40);
+console.log(c);
