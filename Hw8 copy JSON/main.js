064b18d5b6b12816f704a79937b6d76326b7963e
BieliaievMykhailo <bieliaiev.mykhailo@gmail.com>
2024-08-28 21:41:20 +0300

    hw 8 done!

--- Hw8 copy JSON/main.js
+++ Hw8 copy JSON/main.js
@@ -2,29 +2,55 @@
 // Створити функцію, яка робить глибоку копію об'єкту.
 // Додати перевірки на undefined, null, NaN.
 //     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
-let obj1 = {
-    name: 'vasya',
-    age: 21,
-    status: false,
-    greeting: function () {
-        console.log('Hello ' + this.name);
-    }
-}
-console.log(obj1);
+// wrong
+// let obj1 = {
+//     name: 'vasya',
+//     age: 21,
+//     status: false,
+//     greeting: function () {
+//         console.log('Hello ' + this.name);
+//     }
+// }
+// console.log(obj1);
+//
+// function copyObj(a) {
+//     if (a !== null || true || (!isNaN(a))) {
+//         let newObj = {};
+//         newObj.greeting = a.greeting;
+//         let json = JSON.stringify(a);
+//         let newCopy = JSON.parse(json);
+//         newCopy.greeting = newObj.greeting;
+//         return newCopy;
+//     }
+// }
+//
+// let b = copyObj(obj1);
+// console.log(b);
+function cloner(obj) {
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
 
-function copyObj(a) {
-    if (a !== null || true || (!isNaN(a))) {
-        let newObj = {};
-        newObj.greeting = a.greeting;
-        let json = JSON.stringify(a);
-        let newCopy = JSON.parse(json);
-        newCopy.greeting = newObj.greeting;
-        return newCopy;
+        console.log(cloneObj);
+        return cloneObj
+
     }
+
+    throw new Error('!!!!!');
+
 }
 
-let b = copyObj(obj1);
-console.log(b);
+const clone = cloner({id: 123, name: 'asdsad', greeting() {console.log('hello')}, foo() {console.log('bar')}});
+clone.foo();
 //
 // #iz6emEsP2BA
 // - є масив
